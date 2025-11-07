1e16315657a45e9ed92f5f0c3c5769981471b780
bakedpotatolord <jeffy9745@gmail.com>
2025-11-07 10:57:48 -0700

    implement mkdir and splitpath start

--- mkdirSplitpath.c
+++ mkdirSplitpath.c
@@ -6,13 +6,52 @@ extern struct NODE* cwd;
 //make directory
 void mkdir(char pathName[]){
 
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+    char baseName[64];
+    char dirName[64];
 
-    printf("TO BE IMPLEMENTED\n");
+    struct NODE* base = splitPath(pathName,baseName,dirName);
+
+    struct NODE* firstChild = base->childPtr;
+
+    struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+
+    strcpy(newNode->name,baseName);
+    newNode->fileType = 'D';
+    newNode->childPtr = NULL;
+    newNode->siblingPtr = firstChild;
+    newNode->parentPtr = base;
+
+    base->childPtr = newNode;
+
+
 
     return;
+}
+
+
+struct NODE* getChild(struct NODE* directory, char* name){
+
+    struct NODE* child = NULL;
+    struct NODE* iterator = directory->childPtr;
+
+    // printf("Directory name: %s, searching name: %s\n", directory->name,name);
+
+    while(iterator != NULL ){
+        // printf("strcmp Iterator name: <%s> name: <%s>\n", iterator->name,name);
+
+        int res = strcmp(iterator->name, name);
+        // printf("strcmp res: %d\n", res);
+        if( res == 0){
+            child = iterator;
+            break;
+        }
+        iterator = iterator->siblingPtr;
+    }
+    if(child == NULL){
+        printf("ERROR: directory %s does not exist\n", name);
+        return NULL;
+    }
+    return child;
 }
 
 //handles tokenizing and absolute/relative pathing options
@@ -29,6 +68,11 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
     char hasSlash = 0;
     char* token=pathName;
 
+    if(pathName[0] == '/'){
+        token++;
+        pathName++;
+    }
+
     //find end
     for(; *token != '\0'; token++){
             //find start of dirname
@@ -37,7 +81,7 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
         }
     }
 
-    // printf("hasSlash: %d\n",hasSlash);
+    printf("hasSlash: %d\n",hasSlash);
 
     //if no slash
     if(hasSlash == 0){
@@ -65,14 +109,34 @@ struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
 
     }
 
+    printf("split <%s> to base <%s> and DIR <%s>\n",pathName,baseName,dirName);
     // printf("base: %s\n",baseName);
     // printf("dir: %s\n",dirName);
+
+    char dirnameCopy[128];
+    strcpy(dirnameCopy,dirName);
+
+    struct NODE * traversalPtr = root;
+
+    char* splitter = "/";
+
+    if(hasSlash){
+
+        char* dir = strtok(dirnameCopy, splitter);
+
+        while (dir != NULL) {
+            traversalPtr = getChild(traversalPtr, dir);
+            if(traversalPtr == NULL){
+                return NULL;
+            }
+            printf(" %s\n", dir);
+            dir = strtok(NULL, splitter); // Subsequent calls use NULL
+        }
+    }
     
 
-    struct NODE temp = {
-        baseName,
-    }
+    
 
     //
-    return NULL;
+    return traversalPtr;
 }
